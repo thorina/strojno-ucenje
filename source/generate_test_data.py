72bbdc25641432f78d88a4023a95d949113b34b6
gorana <gorana.levacic.zd@gmail.com>
2016-05-14 21:44:37 +0200

    Evil regex, line iterating very good, yes

--- source/generate_test_data.py
+++ source/generate_test_data.py
@@ -1,11 +1,11 @@
 import os
 import re
+import tempfile
 import nltk.data
 # from nltk import word_tokenize
 from nltk.tag import StanfordNERTagger
 
 # current working directory = source
-
 current_path = os.getcwd() + '/lib/nltk_data'
 nltk.data.path.append(current_path)
 
@@ -14,65 +14,80 @@ st = StanfordNERTagger('../lib/stanford-ner/classifiers/english.all.3class.dists
                        encoding='utf-8')
 
 regex_extra_whitespaces = re.compile('( ){2,}')
-regex_tag = re.compile('\[\d+\]')
-regex_songs = re.compile('\"\n')
 regex_footnotes = re.compile('(footnote)s?', flags=re.IGNORECASE)
 regex_footnote = re.compile('\[(footnote)[\w\s:_$&%"\',\-\.\?!\(\)\\\/]+\]', flags=re.IGNORECASE)
 regex_illustration = re.compile('\n*\[(illustration)[\w\s:_$&%"\',\-\.\?!\(\)\\\/]*\]', flags=re.IGNORECASE)
 
-regex_story_separator = \
-    re.compile('\n{3,4}[\w \t:_$&%"\',\-\.\?!\(\)\\\/]+(\n{1,2}[\w \t:_$&%"\',\-\.\?!\(\)\\\/]*){1,2}\n{2,4}')
-
 path = '../data/gutenberg_stripped_files'
-cnt = 0
 
-for filename in os.listdir(path):
+with tempfile.NamedTemporaryFile(mode='w+t') as temp:
+    for filename in os.listdir(path):
+        file_path = path + '/' + filename
+        with open(file_path, 'r') as file:
+            content = file.read()
+            content = regex_extra_whitespaces.sub(' ', content)
+            content = regex_footnote.sub('', content)
+            content = regex_footnotes.sub('', content)
+            content = regex_illustration.sub('', content)
+            temp.write(content)
 
-    file = open(path + '/' + filename, 'r')
-    file_content = file.read()
-    file.close()
+    # story separators are usually in this form:
+    # 2-3 empty lines
+    # 1 short line title or chapter number
+    # optionally: 1-2 empty lines + 1 short line title or author
+    # 2-3 empty lines
 
-    file_content = regex_extra_whitespaces.sub(' ', file_content)
-    file_content = regex_songs.sub('', file_content)
-    file_content = regex_tag.sub('', file_content)
-    file_content = regex_footnote.sub('', file_content)
-    file_content = regex_footnotes.sub('', file_content)
-    file_content = regex_illustration.sub('', file_content)
-    separated_stories = regex_story_separator.split(file_content)
+    story_count = 0
+    file_name = '../data/training/stories/' + str(story_count) + '_story.txt'
+    file = open(file_name, 'w+')
 
-    for story in separated_stories:
+    temp.seek(0)
+    lines = temp.readlines()
+    last_index = len(lines)-6
+    i = 0
+    while i < last_index:
+        before_title = lines[i].isspace() and lines[i + 1].isspace()
+        title = not lines[i + 2].isspace() and len(lines[i + 2]) < 50
+        blank_line_after_title = lines[i + 3].isspace()
+        after_title_variant_one = lines[i + 4].isspace()
+        after_title_variant_two = not lines[i + 4].isspace() and len(lines[i + 4]) < 60 \
+                                  and lines[i + 5].isspace() \
+                                  and lines[i + 6].isspace()
 
-        # regex_story_separator has () so split will return short or empty string that are part of separator
-        # from documentation:
-        # If capturing parentheses are used in pattern, then the text of all groups
-        # in the pattern are also returned as part of the resulting list.
-        if len(story) < 10:
-            continue
+        if before_title and title and blank_line_after_title and (after_title_variant_one or after_title_variant_two):
+            story_count += 1
+            print("story count: " + str(story_count))
+            file.close()
+            file_name = '../data/training/stories/' + str(story_count) + '_story.txt'
+            file = open(file_name, 'w+')
+            if after_title_variant_one:
+                i += 5
+            else:
+                i += 7
+        else:
+            file.write(lines[i])
+            i += 1
 
-        cnt += 1
-        file_name = '../data/training/stories/' + str(cnt) + '_story.txt'
-        file = open(file_name, 'w+')
-        file.write(story)
-        if len(story) < 300:
-            print(str(cnt) + " - " + str(len(story)))
+    for i in range(last_index, len(lines)):
+        file.write(lines[i])
 
-        # tokenized_text = word_tokenize(text)
-        # tagged_text = st.tag(tokenized_text)
-        # characters = []
-        # k = 0
-        # for entity in tagged_text:
-        #     if entity[1] == 'PERSON':
-        #         if entity[0] not in characters:
-        #             characters.append(entity[0])
-        #
-        # character_string = ', '.join(characters)
-        #
-        # file_name = "data/training/stories/" + str(i) + "_story.txt"
-        # fo = open(file_name, "w+")
-        # fo.write(text)
-        # fo.close()
-        #
-        # file_name = "data/training/characters/" + str(i) + "_characters.txt"
-        # fo = open(file_name, "w+")
-        # fo.write(character_string)
-        # fo.close()
+# tokenized_text = word_tokenize(text)
+# tagged_text = st.tag(tokenized_text)
+# characters = []
+# k = 0
+# for entity in tagged_text:
+#     if entity[1] == 'PERSON':
+#         if entity[0] not in characters:
+#             characters.append(entity[0])
+#
+# character_string = ', '.join(characters)
+#
+# file_name = "data/training/stories/" + str(i) + "_story.txt"
+# fo = open(file_name, "w+")
+# fo.write(text)
+# fo.close()
+#
+# file_name = "data/training/characters/" + str(i) + "_characters.txt"
+# fo = open(file_name, "w+")
+# fo.write(character_string)
+# fo.close()
