5818355100a80184fa98eeb9118f08a146410d7f
gorana <gorana.levacic.zd@gmail.com>
2016-06-03 11:29:09 +0200

    Refactor!

--- source/models.py
+++ source/models.py
@@ -1,3 +1,5 @@
+import os
+
 import nltk
 from nltk.tag import StanfordNERTagger
 
@@ -5,6 +7,13 @@ from source.conditional_random_fields import train_crf_model, load_trained_crf_m
 from source.hidden_markov_model import train_hmm_model, load_trained_hmm_model
 from source.labeled_tokens import populate_labeled_tokens
 from source.utils import write_tagged_content_to_file
+
+STANFORD_NER_JAR = '../lib/stanford-ner/stanford-ner.jar'
+STANFORD_NER_MODELS_PATH = '../lib/stanford-ner/classifiers/'
+TRAINED_STANFORD_NER_LOWER_PUNCT = 'trained_stanford_ner_lower_punct.ser.gz'
+TRAINED_STANFORD_NER_LOWER = 'trained_stanford_ner_lower.ser.gz'
+TRAINED_STANFORD_NER_PUNCT = 'trained_stanford_ner_punct.ser.gz'
+TRAINED_STANFORD_NER = 'trained_stanford_ner.ser.gz'
 
 TEST_FILES_PATH = '../data/test-files'
 TAGGED_TEST_FILES_PATH = '../data/test-files/tagged-test-files'
@@ -48,7 +57,7 @@ class Models:
         self.crf_lower = load_trained_crf_model(labeled_tokens_lower, False, True)
         self.crf_lower_punct = load_trained_crf_model(labeled_tokens_lower_punct, True, True)
 
-        self.load_stanford_ner_models()
+        self.load_existing_stanford_ner_models()
 
     def retrain_models(self):
         labeled_tokens = populate_labeled_tokens(False, False)
@@ -72,53 +81,83 @@ class Models:
         write_tagged_content_to_file(labeled_tokens_lower, PATH_TOKENIZED_CONTENT_LOWER)
         write_tagged_content_to_file(labeled_tokens_lower_punct, PATH_TOKENIZED_CONTENT_LOWER_PUNCT)
 
-        print('You should now retrain Stanford NER tagger with following commands from terminal '
-              'run from directory ../lib/stanford-ner:')
-        print('java -mx4g -cp ".*:lib/*:stanford-ner.jar" edu.stanford.nlp.ie.crf.CRFClassifier '
-              '-prop ner.properties -trainFile training-sets/tokenized_content.tsv '
-              '-serializeTo classifiers/trained_stanford_ner.ser.gz')
+        show_retraining_instruction()
+        show_bash_instruction(TRAINED_STANFORD_NER)
+        show_bash_instruction(TRAINED_STANFORD_NER_PUNCT)
+        show_bash_instruction(TRAINED_STANFORD_NER_LOWER)
+        show_bash_instruction(TRAINED_STANFORD_NER_LOWER_PUNCT)
+        self.wait_until_models_are_trained()
 
-        print('java -mx4g -cp ".*:lib/*:stanford-ner.jar" edu.stanford.nlp.ie.crf.CRFClassifier '
-              '-prop ner.properties -trainFile training-sets/tokenized_content_lower.tsv '
-              '-serializeTo classifiers/trained_stanford_ner_lower.ser.gz')
+    def load_existing_stanford_ner_models(self):
+        needs_training = check_if_all_models_exist()
+        if needs_training:
+            self.wait_until_models_are_trained()
+        else:
+            self.load_stanford_ner_models()
 
-        print('java -mx4g -cp ".*:lib/*:stanford-ner.jar" edu.stanford.nlp.ie.crf.CRFClassifier '
-              '-prop ner.properties -trainFile training-sets/tokenized_content_punct.tsv '
-              '-serializeTo classifiers/trained_stanford_ner_punct.ser.gz')
+    def load_stanford_ner_models(self):
+        self.stanford_ner = StanfordNERTagger(
+            STANFORD_NER_MODELS_PATH + TRAINED_STANFORD_NER,
+            STANFORD_NER_JAR,
+            encoding='utf-8')
 
-        print('java -mx4g -cp ".*:lib/*:stanford-ner.jar" edu.stanford.nlp.ie.crf.CRFClassifier '
-              '-prop ner.properties -trainFile training-sets/tokenized_content_lower_punct.tsv '
-              '-serializeTo classifiers/trained_stanford_ner_lower_punct.ser.gz')
+        self.stanford_ner_punct = StanfordNERTagger(
+            STANFORD_NER_MODELS_PATH + TRAINED_STANFORD_NER_PUNCT,
+            STANFORD_NER_JAR,
+            encoding='utf-8')
 
-        print('Please enter ok after the models are trained. If you enter ok before that, old models will be used!')
+        self.stanford_ner_lower = StanfordNERTagger(
+            STANFORD_NER_MODELS_PATH + TRAINED_STANFORD_NER_LOWER,
+            STANFORD_NER_JAR,
+            encoding='utf-8')
+
+        self.stanford_ner_lower_punct = StanfordNERTagger(
+            STANFORD_NER_MODELS_PATH + TRAINED_STANFORD_NER_LOWER_PUNCT,
+            STANFORD_NER_JAR,
+            encoding='utf-8')
+
+    def wait_until_models_are_trained(self):
+        print('Please enter ok when training finishes.')
         print('If you want to exit, enter q.')
         while True:
             input_string = input('--> ')
             if input_string == 'q':
                 quit()
-
             elif input_string == 'ok':
-                self.load_stanford_ner_models()
+                self.load_existing_stanford_ner_models()
                 break
-
             else:
                 print('Incorrect input - please enter ok or q.')
                 continue
 
-    def load_stanford_ner_models(self):
-        self.stanford_ner = StanfordNERTagger('../lib/stanford-ner/classifiers/trained_stanford_ner.ser.gz',
-                                              '../lib/stanford-ner/stanford-ner.jar',
-                                              encoding='utf-8')
 
-        self.stanford_ner_punct = StanfordNERTagger('../lib/stanford-ner/classifiers/trained_stanford_ner_lower.ser.gz',
-                                                    '../lib/stanford-ner/stanford-ner.jar',
-                                                    encoding='utf-8')
+def model_does_not_exist(model):
+    path = STANFORD_NER_MODELS_PATH + model
+    if not os.path.exists(path):
+        show_warning(model)
+        return True
 
-        self.stanford_ner_lower = StanfordNERTagger('../lib/stanford-ner/classifiers/trained_stanford_ner_lower.ser.gz',
-                                                    '../lib/stanford-ner/stanford-ner.jar',
-                                                    encoding='utf-8')
 
-        self.stanford_ner_lower_punct = StanfordNERTagger(
-            '../lib/stanford-ner/classifiers/trained_stanford_ner_lower_punct.ser.gz',
-            '../lib/stanford-ner/stanford-ner.jar',
-            encoding='utf-8')
+def check_if_all_models_exist():
+    ner_missing = model_does_not_exist(TRAINED_STANFORD_NER)
+    ner_punct_missing = model_does_not_exist(TRAINED_STANFORD_NER_PUNCT)
+    ner_lower_missing = model_does_not_exist(TRAINED_STANFORD_NER_LOWER)
+    ner_lower_punct_missing = model_does_not_exist(TRAINED_STANFORD_NER_LOWER_PUNCT)
+    return ner_missing or ner_punct_missing or ner_lower_missing or ner_lower_punct_missing
+
+
+def show_warning(model):
+    print('File ' + model + ' does not exist!')
+    show_retraining_instruction()
+    show_bash_instruction(model)
+
+
+def show_retraining_instruction():
+    print('You should now retrain Stanford NER tagger with following command(s) from terminal '
+          'run from directory ../lib/stanford-ner:')
+
+
+def show_bash_instruction(model):
+    print('java -mx4g -cp ".*:lib/*:stanford-ner.jar" edu.stanford.nlp.ie.crf.CRFClassifier '
+          '-prop ner.properties -trainFile training-sets/tokenized_content.tsv '
+          '-serializeTo ' + model + '\n')
